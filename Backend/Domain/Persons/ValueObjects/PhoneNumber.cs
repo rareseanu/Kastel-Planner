f98bf51c84d3f704d6888a27938f08831f71e08c
Nita Cristina <cristinanita96@yahoo.com>
2021-09-21 21:19:07 +0300

    validation on register form v2

--- Backend/Domain/Persons/ValueObjects/PhoneNumber.cs
+++ Backend/Domain/Persons/ValueObjects/PhoneNumber.cs
@@ -13,18 +13,7 @@ namespace Domain.Persons.ValueObjects
         public string Number { get; }
 
         public static Result<PhoneNumber> Create(string number)
-        {
-            const string phoneNumberRegex = @"^\d{10}$"; // Ex: 0761234567
-
-            if (string.IsNullOrWhiteSpace(number))
-            {
-                return Result.Failure<PhoneNumber>("Phone number was not provided.");
-            }
-
-            if(!Regex.IsMatch(number, phoneNumberRegex))
-            {
-                return Result.Failure<PhoneNumber>("Invalid phone number format.");
-            }
+        {       
 
             return Result.Success(new PhoneNumber(number));
         }
